19ac21621172f5e10a18435124525ac3fd967171
Lucas Barbosa <lucasbdsaint@gmail.com>
2024-02-19 22:22:06 -0300

    simulador de piano

---
+++ simulador de piano/src/scripts/engine.js
@@ -0,0 +1,44 @@
+const pianoKeys = document.querySelectorAll(".piano-keys .key");
+const volumeSlider = document.querySelector(".volume-slider input");
+const keysCheck = document.querySelector(".keys-check input");
+
+
+const mepedKeys = [];
+let audio = new Audio();
+
+const playTune = (key) => {
+    audio.src = `./src/tunes/${key}.wav`;
+    audio.play();
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add("active");
+
+    setTimeout(() => {
+        clickedKey.classList.remove("active");
+    }, 150);
+}
+
+pianoKeys.forEach(key => {
+    key.addEventListener("click", () => playTune(key.dataset.key));
+    mepedKeys.push(key.dataset.key);
+})
+
+document.addEventListener("keydown", (event) => {
+
+    if (mepedKeys.includes(event.key)) {
+        playTune(event.key);
+    }
+})
+
+
+const showHideKeys = () => {
+    pianoKeys.forEach(key => key.classList.toggle("hide"));
+}
+
+const handleVolume = (event) => {
+    audio.volume = event.target.value;
+}
+
+volumeSlider.addEventListener("input", handleVolume);
+
+keysCheck.addEventListener("change", showHideKeys);
